60b325864cfd71ddd53c757688167f539d074bf7
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-09-11 00:56:56 +0530

    update k means

--- 01-K-Means/k-means.py
+++ 01-K-Means/k-means.py
@@ -1,11 +1,45 @@
-import pathlib
-import pandas as pd
-import numpy as np
+def euclid_distance(pointA, pointB, size: int = 2) -> float :
+    """A method to calculate the euclidean distance between two points
 
-DATASET_URI = f"{pathlib.Path().resolve()}\\..\\Dataset\\IRIS\\iris.data"
-DATASET_HEADERS = ['sepal length', 'sepal width', 'petal length', 'petal width', 'type']
+    Parameters
+    ----------
+    pointA : Any
+        The first point
+    pointB : Any
+        The second point
+    size : int, optional
+        The dimension of the points or the number of elements in the coordinates. Default value is 2.
 
-datasetFile = open(DATASET_URI)
-dataFrame = pd.read_csv(datasetFile, header=None, names=DATASET_HEADERS)
+    Returns
+    -------
+    float
+        The euclidean distance between the points
+    """
 
-print(dataFrame)
+    sqDistance = 0.0
+
+    for i in range(size) :
+        sqDistance += pow(pointA[i] - pointB[i], size)
+
+    distance = float(pow(sqDistance, 1.0 / size))
+    return distance
+
+
+
+if __name__ == "__main__" :
+    k = int(input("Enter the number of clusters: "))
+
+    import pathlib
+    import pandas as pd
+
+    DATASET_URI = f"{pathlib.Path().resolve()}\\..\\Dataset\\IRIS\\iris.data"
+    DATASET_HEADERS = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'type']
+
+    datasetFile = open(DATASET_URI)
+    dataFrame = pd.read_csv(datasetFile, header=None, names=DATASET_HEADERS)
+    datasetFile.close()
+
+    data_amount = dataFrame.shape[0]
+
+    import random
+    cluster_indices = random.sample(range(data_amount), k)
