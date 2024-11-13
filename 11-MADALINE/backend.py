b8085b73f5ca32922b0db58e3eedd8838aded0a6
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-11-13 20:45:20 +0530

    add ann assignment

--- 11-MADALINE/backend.py
+++ 11-MADALINE/backend.py
@@ -11,41 +11,60 @@ class MADALINE :
         self.layer_data = layer_data
 
         # Initialize layers
-        layer_in = []
-        layer_out = []
-        bias_weight = []
+        self.layer_in = []
+        self.layer_out = []
+        self.bias_weight = []
         for i in range(num_layers) :
             layer = np.zeros((layer_data[i], 1))
             layer_b = np.zeros((layer_data[i], 1))
-            layer_c = np.ones((layer_data[i], 1))
-            layer_in.append(layer)
-            layer_out.append(layer_b)
-            bias_weight.append(layer_c)
+            layer_c = np.random.uniform(-0.5, 0.5, (layer_data[i], 1))
+            self.layer_in.append(layer)
+            self.layer_out.append(layer_b)
+            self.bias_weight.append(layer_c)
 
         # Initialize weight matrices
-        weight_mats = []
+        self.weight_mats = []
         for i in range(num_layers-1) :
             layer = np.random.uniform(-0.5, 0.5, (layer_data[i+1], layer_data[i]))
-            weight_mats.append(layer)
+            self.weight_mats.append(layer)
         
         # TODO: implement parameter calculation
 
-    def compute(x_data : np.ndarray, y_data : np.ndarray, epochs : int) :
+    def compute(self, x_data : np.ndarray, y_data : np.ndarray, epochs : int) :
         """
         Method to fit data to the network
         """
         
         data_count = x_data.shape[0]
         current_epoch = 0
+        fitting = True
 
-        with tqdm(total=data_count) as pbar :
+        while current_epoch - 1 < epochs and fitting :
+            with tqdm(total=data_count) as pbar :
 
-            def row_iter(x) :
-                current_epoch += 1
+                def row_iter(x : np.ndarray) :
+                    current_epoch += 1
 
-                pbar.set_description(f"Epoch {current_epoch}")
-            
-            np.apply_along_axis(row_iter, 1, x_data)
+                    for i in range(self.num_layers) :
+                        if i == 0 :
+                            self.layer_in[i] = x.T
+                            self.layer_out[i] = x.T
+                            continue
+
+                        intermediate_array = np.matmul(self.weight_mats[i-1], self.layer_out[i-1])
+                        self.layer_in[i] = np.add(intermediate_array, self.bias_weight[i])
+
+                        for j in range(self.layer_in[i].shape[0]) :
+                            self.layer_out[i][j] = 1 if self.layer_in[i][j] >= 0 else -1
+
+                    if np.array_equal(y_data[current_epoch - 1], self.layer_out[self.num_layers - 1]) :
+                        ...
+
+                    pbar.set_description(f"Epoch {current_epoch}")
+                    pbar.set_postfix_str("")
+                    pbar.update(1)
+                
+                np.apply_along_axis(row_iter, 1, x_data)
 
 
 def create_madaline(topology : str) :
