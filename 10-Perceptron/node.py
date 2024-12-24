f7a4000a74dc7f99aa213fb0bbdf118f47f125bf
ShambaC <38806897+ShambaC@users.noreply.github.com>
2024-12-24 11:43:50 +0530

    add csv inputs for perceptron

--- 10-Perceptron/node.py
+++ 10-Perceptron/node.py
@@ -4,33 +4,16 @@ import pandas as pd
 from backend import Perceptron
 from sklearn.model_selection import train_test_split
 
-X = np.array([
-    [-1, -1, -1],
-    [-1, -1, 1],
-    [-1, 1, -1],
-    [-1, 1, 1],
-    [1, -1, -1],
-    [1, -1, 1],
-    [1, 1, -1],
-    [1, 1, 1]
-])
+X = pd.read_csv('3_NAND.csv')
+# X = pd.read_csv('3_NOR.csv')
+# X = pd.read_csv('3_XOR.csv')
+# X = pd.read_csv('3_XNOR.csv')
 
-y = np.array([
-    [-1],
-    [1],
-    [1],
-    [-1],
-    [1],
-    [-1],
-    [-1],
-    [1]
-])
-
-weights = np.array([0, 0, 0, 0])
+y = X.pop('output')
 
 percept = Perceptron(X.shape[1])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-percept.fit(X_train, y_train, 50, 0.5, 1)
+percept.fit(X_train, y_train, 20, 1, 1)
 percept.evaluate(X_test, y_test)
